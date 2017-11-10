3dfac56485eefd1a5f50d7c52e1f15b4f7e45339
Rajshall <rajshall@yahoo.com>
2017-11-10 22:27:32 +0000

    Initial Commit

---
+++ JenkinsSampleClassLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace JenkinsSampleClassLibrary
+{
+    public class Program
+    {
+        public bool Main(string isSuccess)
+        {
+            bool conversionPossible;           
+
+            var result = bool.TryParse(isSuccess, out conversionPossible);
+
+            if (conversionPossible)
+            {
+                return result;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ JenkinsSampleClassLibraryTests/ProgamTest.cs
@@ -0,0 +1,19 @@
+﻿using JenkinsSampleClassLibrary;
+using NUnit.Framework;
+
+namespace JenkinsSampleClassLibraryTests
+{
+    [TestFixture]
+    public class ProgamTest
+    {
+        [Test]
+        public void MainTest_ConvertAndReturnString()
+        {
+            var program = new Program();
+
+            var result = program.Main("false");
+
+            Assert.AreEqual(false, result);
+        }
+    }
+}
